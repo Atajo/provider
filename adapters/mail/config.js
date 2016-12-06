6230f6d025d7bfd577e933a65c922caeef930882
k1dbl4ck <k1dbl4ck@gmail.com>
2016-12-06 12:38:20 +0200

    removed invalid SES keys from repo

--- adapters/mail/config.js
+++ adapters/mail/config.js
@@ -1,12 +1,6 @@
 module.exports = {
 
 
-    SES: {
-        apiVersion: '2012-10-17',
-        accessKeyId: "AKIAJNUUNL2NZ5V2IIGQ",
-        secretAccessKey: "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",
-        region: "us-east-1"
-    },
 
 
 
--- adapters/mail/mailer.js
+++ adapters/mail/mailer.js
@@ -1,6 +1,6 @@
 var nodemailer = require('nodemailer');
 var sesTransport = require('nodemailer-ses-transport');
-var config = require('./config');
+var config = require('../../../config/config.json');
 var AWS = require('aws-sdk');
 var atajo = { log: require('../../lib/atajo.log').init('ADAPTER:MAIL', 'mailAdapter.log') };
 
@@ -11,7 +11,14 @@ module.exports = {
     send: function(options, cb) {
 
 
-        var ses = new AWS.SES(config.SES);
+        if (!config.AWS || !config.AWS.SES) {
+            atajo.log.e("CANNOT SEND MAIL. PLEASE CONFIGURE AMAZON SES SETTINGS IN PROVIDER/CONFIG/CONFIG.JSON");
+            return;
+        }
+
+
+
+        var ses = new AWS.SES(config.AWS.SES);
         var transporter = nodemailer.createTransport(sesTransport({ ses: ses }));
 
         atajo.log.d("SENDING MAIL WITH OPTIONS : " + JSON.stringify(options));
