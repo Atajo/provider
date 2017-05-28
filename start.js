cd986d1d2e53ac4299bfcf1fb78bda161c2eca3e
k1dbl4ck <k1dbl4ck@gmail.com>
2017-05-29 01:04:27 +0200

    feat: multi socket interface

--- start.js
+++ start.js
@@ -1,3 +1,7 @@
+var os = require('os');
+var fork = require('child_process').fork;
+var path = require('path');
+
 require('./lib/atajo.env').init(function() {
 
     require('./lib/atajo.provider').init(function(atajo) {
@@ -23,7 +27,29 @@ require('./lib/atajo.env').init(function() {
 
                 atajo.log.d("CONNECTING TO : " + URI + " (" + RELEASE + ")");
 
-                atajo.io.init(RELEASE, URI);
+                var len = os.cpus().length;
+
+                atajo.log.d("SPAWINING " + len + " IO INTERFACES");
+
+                for (var i = 0; i < len; i++) {
+
+
+                    var HANDLER = {
+                        process: fork(path.join(__dirname, './', 'lib', 'atajo.io.js')),
+                        processId: i,
+                        release: RELEASE
+                    }
+
+                    HANDLER.process.on('message', function(msg) {
+
+
+                    });
+
+                    //SEND REQUEST TO PROCESS
+                    HANDLER.process.send({ processId: HANDLER.processId, release: HANDLER.release, uri: URI });
+
+
+                }
 
 
             });
