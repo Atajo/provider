5d9c83ff582a5af31c2a5476a9b30eb6b60d02be
root <root@supqa.mobs.int>
2017-10-03 11:43:11 +0200

    fix: update SAP adapter + update nodemailer version

--- adapters/sap/adapter.js
+++ adapters/sap/adapter.js
@@ -6,18 +6,20 @@ var rfc = null;
 var config = null;
 
 
-var adapter = {
+class SAPAdapter {
 
-    CONNECTION_PARAMETERS: null,
 
-    init: function() {
+    constructor() {
 
         atajo.log.d("INITIALIZING SAP ADAPTER");
 
         try {
 
             //rfc = require('node-rfc');
-            rfc = require('sapnwrfc');
+            let rfc = require('sapnwrfc');
+            this.client = new rfc.Connection;
+
+
 
 
         } catch (e) {
@@ -29,7 +31,9 @@ var adapter = {
 
         try {
 
-            config = require('../../../conf/sap.json');
+            let config = require('../../../conf/sap.json');
+            this.CONNECTION_PARAMETERS = config[GLOBAL.RELEASE];
+
 
         } catch (e) {
 
@@ -39,131 +43,89 @@ var adapter = {
         }
 
 
-        this.CONNECTION_PARAMETERS = config[GLOBAL.RELEASE];
 
-        return this;
+    }
 
-    },
+    query(bapi, obj, commit, at) {
 
-    query: function(bapi, obj, commit, at) {
-
-
+        atajo.log.d("SAP.QUERY : " + bapi + " -> " + JSON.stringify(obj));
+        let that = this;
         at = at || new Date().getTime();
         commit = commit || false;
+        let qry = { bapi: bapi, obj: obj, commit: commit, at: at }
 
-        var qry = { bapi: bapi, obj: obj, commit: commit, at: at }
 
-
-        var _worker = Object.create(adapter.worker);
-
-        return _worker.process(qry);
+        return new Promise((resolve, reject) => {
 
 
 
-    },
+            atajo.log.d("SAP.QUERY RUNNING : " + JSON.stringify(qry));
 
+            if (!qry) {
+                return reject({ status: 0, message: "INVALID REQUEST", result: "" });
+            } else if (!qry.bapi) {
+                return reject({ status: 0, message: "INVALID REQUEST - NO BAPI NAME SET", result: "" });
+            } else if (!qry.obj) {
+                return reject({ status: 0, message: "INVALID REQUEST - NO BAPI DATA SET", result: "" });
+            }
 
-    worker: {
+            var bapi = qry.bapi;
+            var obj = qry.obj;
+            var commit = qry.commit;
+            //atajo.log.d("CONNECTION PARAMETERS ARE : " + JSON.stringify(adapter.CONNECTION_PARAMETERS));
+            atajo.log.d("CALLING BAPI " + bapi + " (COMMIT : " + commit + ") WITH DATA : " + JSON.stringify(obj).substring(0, 100) + '...');
 
-        client: null,
+            this.client.Open(this.CONNECTION_PARAMETERS, (err) => {
 
-        process: function(q) {
-            var that = this;
-
-            return new Promise(function(resolve, reject) {
-
-                if (!q) {
-                    return reject({ status: 0, message: "INVALID REQUEST", result: "" });
-                } else if (!q.bapi) {
-                    return reject({ status: 0, message: "INVALID REQUEST - NO BAPI NAME SET", result: "" });
-                } else if (!q.obj) {
-                    return reject({ status: 0, message: "INVALID REQUEST - NO BAPI DATA SET", result: "" });
+                if (err) { // check for login/connection errors
+                    atajo.log.e("SAP CLIENT CONNECT ERROR : " + err);
+                    return reject({ status: 0, message: "ERROR CONNECTING TO SAP BACKEND @ " + conParams.ashost, result: err });
                 }
 
-                var bapi = q.bapi;
-                var obj = q.obj;
-                var commit = q.commit;
-                that.client = new rfc.Connection;
-                //atajo.log.d("CONNECTION PARAMETERS ARE : " + JSON.stringify(adapter.CONNECTION_PARAMETERS));
-                atajo.log.d("CALLING BAPI " + bapi + " (COMMIT : " + commit + ") WITH DATA : " + JSON.stringify(obj).substring(0, 100) + '...');
+                var func = that.client.Lookup(bapi);
+                func.Invoke(obj, (err, result) => {
+                    if (err) { // check for errors (e.g. wrong parameters)
+                        atajo.log.e("SAP CLIENT INVOKE ERROR : " + JSON.stringify(err) + "/" + JSON.stringify(result));
 
-                that.client.Open(adapter.CONNECTION_PARAMETERS, function(err) {
-                    //that.client.connect(function(err) {
-                    if (err) { // check for login/connection errors
-                        atajo.log.e("SAP CLIENT CONNECT ERROR : " + err);
-                        return reject({ status: 0, message: "ERROR CONNECTING TO SAP BACKEND @ " + conParams.ashost, result: err });
+                        atajo.log.e(JSON.stringify(obj));
+                        return reject({ status: 0, message: "ERROR INVOKING RFC FOR " + bapi, result: err });
                     }
 
-                    var func = that.client.Lookup(bapi);
+                    atajo.log.d('====== RESULT FOR BAPI ' + bapi + ' IS ==========================');
+                    atajo.log.d(JSON.stringify(result).substring(0, 100) + '...');
+                    atajo.log.d('================================================================================');
 
-                    func.Invoke(obj, function(err, result) {
-                        if (err) { // check for errors (e.g. wrong parameters)
-                            atajo.log.e("SAP CLIENT INVOKE ERROR : " + JSON.stringify(err) + "/" + JSON.stringify(result));
+                    if (commit) {
+                        atajo.log.d("TRANSACTION COMMIT");
+                        bapi = "BAPI_TRANSACTION_COMMIT";
+                        obj = { WAIT: 'X' }
 
-                            atajo.log.e(JSON.stringify(obj));
-                            return reject({ status: 0, message: "ERROR INVOKING RFC FOR " + bapi, result: err });
-                        }
+                        func = that.client.Lookup(bapi);
+                        func.Invoke(obj, function(err, commitResult) {
 
-                        atajo.log.d('====== RESULT FOR BAPI ' + bapi + ' IS ==========================');
-                        atajo.log.d(JSON.stringify(result).substring(0, 100) + '...');
-                        atajo.log.d('================================================================================');
+                            if (err) {
+                                atajo.log.e("SAP CLIENT [COMMIT] INVOKE ERROR : " + err);
+                                reject({ status: 0, message: "COMMIT FAILED : " + err, result: result, commitResult: false });
+                            } else {
+                                resolve({ status: 1, message: "COMMIT SUCCESS", result: result, commitresult: commitResult });
+                            }
 
-                        if (commit) {
-                            atajo.log.d("TRANSACTION COMMIT");
-                            bapi = "BAPI_TRANSACTION_COMMIT";
-                            obj = { WAIT: 'X' }
-
-                            func = that.client.Lookup(bapi);
-                            func.Invoke(obj, function(err, commitResult) {
-
-                                if (err) {
-                                    atajo.log.e("SAP CLIENT [COMMIT] INVOKE ERROR : " + err);
-                                    reject({ status: 0, message: "COMMIT FAILED : " + err, result: result, commitResult: false });
-                                } else {
-                                    resolve({ status: 1, message: "COMMIT SUCCESS", result: result, commitresult: commitResult });
-                                }
-
-                            });
-                        } else {
-                            resolve({ status: 1, message: "TRANSACTION SUCCESS", result: result });
-                        }
-
-                    });
-
+                        });
+                    } else {
+                        resolve({ status: 1, message: "TRANSACTION SUCCESS", result: result });
+                    }
 
                 });
 
 
-
-
-
-
             });
 
-
-
-            /*
-                    if (at) {
-
-                        now = new Date().getTime();
-                        delay = parseInt(at) - parseInt(now);
-                        atajo.log.d("CALL TO " + bapi + " DELAYED - CALLING IN " + delay + "ms");
-                        if (delay > 0) { setTimeout(function() { that.call(q); }, delay); } else { that.call(q); }
-
-                    } else {
-                        that.call(q);
-                    }
-            */
-        },
-
-
-
-
-
-
+        });
 
     }
 
+
 }
 
-module.exports = adapter;
+
+module.exports = new SAPAdapter();
